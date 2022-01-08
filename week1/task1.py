2389eede5af21be06599a0f2b90ba1367f3df07f
Fredy Hernandez <hfredy717@outlook.com>
2022-01-07 19:33:16 -0500

    :sparkles: CRC: team work 1

--- week1/task1.py
+++ week1/task1.py
@@ -1,7 +1,9 @@
-def favoriteColor():
-    name=input('what is your name: ')
-    lastName=input('what is your last name: ')
-    color=input('Please type your favorite color: ')
-    return 'Hi! \nYou are %s %s and your favorite color is: "%s".' % (name.capitalize(), lastName.capitalize(),color)
+def favorite_color():
+    first_name = input('what is your name: ')
+    last_name = input('what is your last name: ')
+    color = input('Please type your favorite color: ')
+    return 'Hi! \nYou are %s %s and your favorite color is: "%s".' \
+           % (first_name.capitalize(), last_name.capitalize(), color)
 
-print(favoriteColor())
+
+print(favorite_color())
