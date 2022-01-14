7521c612e6639a5b16851acc0f30cec4cf8f0b13
Fredy Hernandez <hfredy717@outlook.com>
2022-01-13 21:22:25 -0500

    :fire: remove IDE files

--- week1/task1.py
+++
@@ -1,9 +0,0 @@
-def favorite_color():
-    first_name = input('what is your name: ')
-    last_name = input('what is your last name: ')
-    color = input('Please type your favorite color: ')
-    return 'Hi! \nYou are %s %s and your favorite color is: "%s".' \
-           % (first_name.capitalize(), last_name.capitalize(), color)
-
-
-print(favorite_color())
